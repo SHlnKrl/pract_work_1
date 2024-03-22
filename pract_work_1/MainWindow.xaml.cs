578c4c3e241f57497538a54b5bc36839e41d5f90
SHlnKrl <blackmazafaker@gmail.com>
2024-03-22 02:59:04 +0200

    Добавьте файлы проекта.

---
+++ pract_work_1/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Media.Media3D;
+
+namespace pract_work_1
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            BitmapImage texture = new BitmapImage();
+            texture.BeginInit();
+            texture.UriSource = new Uri("texture.jpg", UriKind.Relative);
+            texture.EndInit();
+
+            ((DiffuseMaterial)((GeometryModel3D)((ModelVisual3D)viewport3D.Children[3]).Content).Material).Brush = new ImageBrush(texture);
+            ((DiffuseMaterial)((GeometryModel3D)((ModelVisual3D)viewport3D.Children[4]).Content).Material).Brush = new ImageBrush(texture);
+        }
+    }
+}
